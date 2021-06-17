44aeee54fd01bb7b9b2470d76f0fc6527f6f497d
Your Name <mikayelnahapetyan@gmail.com>
2021-06-17 21:38:45 +0400

    write func setDetails

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+
+function setDetails(){
+    'use strict'
+}
+
+147
