0c83af2ec4e319fcb89b81a2f735c70ab9d7d2eb
Your Name <mikayelnahapetyan@gmail.com>
2021-06-20 21:26:34 +0400

    return values from func

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,26 @@ var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 
 
-function setDetails(){
+function setDetails(imageUrl, titleText){
     'use strict'
     var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
-    detailImage.setAttribute('src','img/otter3.jpg')
+    detailImage.setAttribute('src',imageUrl)
 
     var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
-    detailTitle.textContent = "You Should Be Dancing";
+    detailTitle.textContent = titleText;
 }
 
+function imageFromThumb(thumbnail){
+    'use strict'
+    return thumbnail.getAttribute('data-image-url')
+}
+
+function titleFromThumb(thumbnail){
+    'use strict'
+    return thumbnail.getAttribute('data-image-title')
+}
+
+function setDetailsFromThumb(thumbnail){
+    'use strict'
+    setDetails(imageFromThumb(thumbnail),titleFromThumb(thumbnail));
+}
